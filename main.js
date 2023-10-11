430bfe7829993dc48ed7ca1fc937b4a438cf65d0
Kyla Yujiri <27860923+kylayujiri@users.noreply.github.com>
2023-10-11 15:06:03 -0400

    Finish my first app

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('Hello from Electron 👋');
+// importing two electron modules
+const { app, BrowserWindow } = require('electron');
+
+const createWindow = () => {
+
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600
+  });
+
+  win.loadFile('index.html');
+
+};
+
+// on windows and linux, closing all windows means quitting the app
+app.on('window-all-closed', () => {
+  if (process.platform != 'darwin') app.quit();
+});
+
+app.whenReady().then(() => {
+  createWindow();
+
+  // on mac, activating an app with no open windows opens a new one
+  app.on('activate', () => {
+    if (BrowserWindow.getAllWindows().length === 0) createWindow();
+  });
+});
